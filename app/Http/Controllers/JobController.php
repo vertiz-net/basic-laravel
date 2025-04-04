6787c7679d3d23d6cc75dfffa6a0bea1b3d9b127
Carlos Vertiz <cvertiz@thumbstopper.com>
2025-04-04 14:54:38 +0200

    Added mail class for sending job posted notification

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\JobPosted;
 use App\Models\Job;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Mail;
 
 class JobController extends Controller
 {
@@ -38,11 +40,13 @@ class JobController extends Controller
             'salary' => ['required'],
         ]);
     
-        Job::create([
+        $job = Job::create([
             'title' => request('title'),
             'salary' => request('salary'),
             'employer_id' => 1
         ]);
+
+        Mail::to($job->employer->user)->send(new JobPosted($job));
     
         return redirect('/jobs');
     }
---
+++ resources/views/mail/job-posted.blade.php
@@ -0,0 +1,11 @@
+<x-mail::message>
+<h2>{{ $job->title }}</h2>
+
+<p>
+    Congrats! Your job is now live on our website.
+</p>
+
+<p>
+    <a href="{{url('/jobs/'.$job->id)}}">View Your Job Listing</a>
+</p>
+</x-mail::message>
