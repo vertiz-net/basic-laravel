31f62125cad4a60e2f78fcdcfe8039239799d7df
Carlos Vertiz <cvertiz@thumbstopper.com>
2025-04-04 15:09:13 +0200

    From sync to queue

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -46,7 +46,7 @@ class JobController extends Controller
             'employer_id' => 1
         ]);
 
-        Mail::to($job->employer->user)->send(new JobPosted($job));
+        Mail::to($job->employer->user)->queue(new JobPosted($job));
     
         return redirect('/jobs');
     }
