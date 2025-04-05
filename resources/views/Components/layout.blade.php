0009ced9fe1a49e1f1c25e397f32c688a69f3c00
Carlos Vertiz <cvertiz@thumbstopper.com>
2025-04-05 15:42:46 +0200

    Added Vite

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Page</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite(['resources/css/app.css'])
+    @vite(['resources/js/app.js'])
+    <!-- <script src="https://cdn.tailwindcss.com"></script> -->
 </head>
 <body class="h-full">
 <div class="min-h-full">
